e1b020098bbd69d32877c9a5482d9db521211236
zack <holyzack1123@hotmail.com>
2024-06-02 23:06:07 +0800

    Add publish time

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,6 +45,8 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
         },
         blog: {
           showReadingTime: true,
