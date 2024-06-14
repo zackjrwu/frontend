45bf811d471f89c42957618b5ad1147b581f94f3
zack <holyzack1123@hotmail.com>
2024-06-14 23:51:19 +0800

    Revise What is React Hook page demo code to component

--- src/components/reactThings/WhatIsReact.js
+++ src/components/reactThings/WhatIsReact.js
@@ -1,19 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const WhatIsReact = () => {
-    const [count, setCount] = useState(0);
-    const handleIncrement = () => {
-      setCount((prev) => prev+1);
-    }
-    return (
-      <div>
-          <h1>Hello, React!</h1>
-          <p>You clicked {count} times</p>
-          <button onClick={handleIncrement}>
-            Click me
-          </button>
-        </div>
-    );
+  const [count, setCount] = useState(0);
+  const handleIncrement = () => {
+    setCount((prev) => prev + 1);
+  };
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>You clicked {count} times</p>
+      <button onClick={handleIncrement}>Click me</button>
+    </div>
+  );
 };
 
 export default WhatIsReact;
